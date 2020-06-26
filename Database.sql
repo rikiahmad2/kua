56693426f770605e64c16f653e1fa6e22f100b7c
rikiahmad2 <ahmadriki9512@gmail.com>
2020-06-26 15:33:17 +0700

    Commit
    Commit

--- Database.sql
+++ Database.sql
@@ -34,12 +34,12 @@ CREATE TABLE IF NOT EXISTS `detail_periode` (
   PRIMARY KEY (`kode_detail`),
   KEY `FK_detail_periode_periode` (`kode_kepala`),
   CONSTRAINT `FK_detail_periode_periode` FOREIGN KEY (`kode_kepala`) REFERENCES `periode` (`kode_kepala`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table kua.detail_periode: ~0 rows (approximately)
 /*!40000 ALTER TABLE `detail_periode` DISABLE KEYS */;
 INSERT INTO `detail_periode` (`kode_detail`, `kode_kepala`, `nomor`, `foto`, `nip`, `ttl`, `kewarganegaraan`, `agama`, `email`, `alamat`, `jabatan`, `prestasi`, `riwayat_pendidikan`) VALUES
-	(1, 10, '1', 'kepalaKUA.jpg', '196809262005011001', 'Bekasi, 26 September 1968', 'Indonesia', 'islam', 'Test@gmail.com', 'Kp. Pelaukan RT.002 RW.002 Desa Karang Rahayu Keca', 'Kepala KUA Sukakarya', NULL, '-MI Anwarul Falah -MTs Anwarul Falah  - MA Anwarul Falah - IAIN SGD Bandung');
+	(11, 16, '666667777777', 'foto7810059.jpg', '123123', '1231-03-12', '123123123', '12312311', '23123123', '1231312', '123123', '123123', '123123');
 /*!40000 ALTER TABLE `detail_periode` ENABLE KEYS */;
 
 -- Dumping structure for table kua.periode
@@ -50,16 +50,13 @@ CREATE TABLE IF NOT EXISTS `periode` (
   `awal` varchar(50) DEFAULT NULL,
   `akhir` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`kode_kepala`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 
--- Dumping data for table kua.periode: ~5 rows (approximately)
+-- Dumping data for table kua.periode: ~2 rows (approximately)
 /*!40000 ALTER TABLE `periode` DISABLE KEYS */;
 INSERT INTO `periode` (`kode_kepala`, `nomor`, `nama`, `awal`, `akhir`) VALUES
-	(6, 1, 'Drs. H.Bunyamin', 'Juni 2004', 'Mei 2009'),
-	(7, 2, 'H.Anas Yazid Fatoni, S.Ag', 'Mei 2009', 'April 2012'),
-	(8, 3, 'H.Amir Mahmud, S.Ag', 'Mei 2012', 'April 2016'),
-	(9, 4, 'Drs. H. Komarudin, MM', 'Mei 2016', 'September 2019'),
-	(10, 5, 'Drs. H.Nanun Abdillah', 'Oktober 2019', 'Sekarang');
+	(16, 1, 'BERAT', 'Mei 2020', 'Januari 2021'),
+	(18, 2, 'PRIA', 'Mei 2019', 'Januari 2021');
 /*!40000 ALTER TABLE `periode` ENABLE KEYS */;
 
 -- Dumping structure for table kua.user
@@ -74,7 +71,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Dumping data for table kua.user: ~1 rows (approximately)
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 INSERT INTO `user` (`id_user`, `nama`, `email`, `password`) VALUES
-	(1, 'ROYAN', 'ahmadriki9512@gmail.com', '1234');
+	(1, 'Sirah Nabi', 'ahmadriki9512@gmail.com', 'admin');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
