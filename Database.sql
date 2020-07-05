cdcf294849d6be930020728c9fc9cdcfb636e7d6
rikiahmad2 <ahmadriki9512@gmail.com>
2020-07-05 15:48:21 +0700

    update
    update

--- Database.sql
+++ Database.sql
@@ -24,12 +24,12 @@ CREATE TABLE IF NOT EXISTS `berita` (
   `title` varchar(50) DEFAULT NULL,
   `isi` text,
   PRIMARY KEY (`kode_berita`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
--- Dumping data for table kua.berita: ~1 rows (approximately)
+-- Dumping data for table kua.berita: ~2 rows (approximately)
 /*!40000 ALTER TABLE `berita` DISABLE KEYS */;
 INSERT INTO `berita` (`kode_berita`, `nomor`, `foto`, `title`, `isi`) VALUES
-	(8, 'Berita Ke 1', 'foto4199830.jpg', 'Cara Menanam Ubi Yang Baik', ' Test Isi Berita Ini ');
+	(9, 'Berita ke 2', 'foto6896058.jpg', 'Cara Ternak Lele', 'Cara Ternah Lele yang baik adalah dengan menggunakan metode yang bernama penanaman bibit di mulut nya');
 /*!40000 ALTER TABLE `berita` ENABLE KEYS */;
 
 -- Dumping structure for table kua.detail_periode
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `detail_periode` (
 -- Dumping data for table kua.detail_periode: ~2 rows (approximately)
 /*!40000 ALTER TABLE `detail_periode` DISABLE KEYS */;
 INSERT INTO `detail_periode` (`kode_detail`, `kode_kepala`, `nomor`, `foto`, `nip`, `ttl`, `kewarganegaraan`, `agama`, `email`, `alamat`, `jabatan`, `prestasi`, `riwayat_pendidikan`) VALUES
-	(11, 16, '666667777777', 'foto7810059.jpg', '123123', '1231-03-12', '123123123', '12312311', '23123123', '1231312', '123123', '123123', '123123');
+	(11, 16, '1', 'foto6203919.jpg', '196809262005011001', '1968-09-26', 'Indonesia', 'Islam', '-', 'Bekasi', 'Kepala KUA Sukakarya', '-', '- MI Anwarul Falah -Mts Anwarul Falah - MA Anwarul Falah - IAIN UIN SGD Bandung');
 /*!40000 ALTER TABLE `detail_periode` ENABLE KEYS */;
 
 -- Dumping structure for table kua.karyawan
@@ -68,15 +68,24 @@ CREATE TABLE IF NOT EXISTS `karyawan` (
   `jabatan` varchar(50) NOT NULL DEFAULT '0',
   `pend_terakhir` varchar(50) NOT NULL DEFAULT '0',
   PRIMARY KEY (`kode_karyawan`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
--- Dumping data for table kua.karyawan: ~0 rows (approximately)
+-- Dumping data for table kua.karyawan: ~8 rows (approximately)
 /*!40000 ALTER TABLE `karyawan` DISABLE KEYS */;
+INSERT INTO `karyawan` (`kode_karyawan`, `nomor`, `nama`, `nip`, `pangkat`, `jabatan`, `pend_terakhir`) VALUES
+	(1, 1, 'Drs. H. Nanun Abdillah', '196809262005011001', 'Penata Tk. 1 lll/d', 'Kepala', 'S.1'),
+	(2, 2, 'Epih Sartika, SE', '197907102005011006', 'Penata Muda Tk.1 lll/b', 'Pengadministrasi', 'S.1'),
+	(3, 3, 'Mimin Munawaroh', '197112181993032001', 'Penata Muda Tk.1 lll/b', 'Pengadministrasi', 'SLTA'),
+	(4, 4, 'Nursinah', '196405302014112001', 'Juru l/c', 'Penyuluh Fungsional', 'SLTP'),
+	(5, 5, 'Akbar Sobari', '', '', 'Honorer', 'S.1'),
+	(6, 6, 'Tono Kartawijaya', '', '', 'Honorer', 'S.1'),
+	(7, 7, 'Ratih Sri Rahayu Wulandari', '', '', 'Honorer', 'SLTA'),
+	(8, 8, 'Nurlela', '', '', 'OB', 'SD');
 /*!40000 ALTER TABLE `karyawan` ENABLE KEYS */;
 
 -- Dumping structure for table kua.masjid
 CREATE TABLE IF NOT EXISTS `masjid` (
-  `id_masjid` int(11) NOT NULL,
+  `id_masjid` int(11) NOT NULL AUTO_INCREMENT,
   `desa` varchar(200) DEFAULT NULL,
   `masjid` int(11) DEFAULT '0',
   `langgar` int(11) DEFAULT '0',
@@ -85,12 +94,18 @@ CREATE TABLE IF NOT EXISTS `masjid` (
   `vihara` int(11) DEFAULT '0',
   `pura` int(11) DEFAULT '0',
   PRIMARY KEY (`id_masjid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
--- Dumping data for table kua.masjid: ~1 rows (approximately)
+-- Dumping data for table kua.masjid: ~6 rows (approximately)
 /*!40000 ALTER TABLE `masjid` DISABLE KEYS */;
 INSERT INTO `masjid` (`id_masjid`, `desa`, `masjid`, `langgar`, `mushola`, `gereja`, `vihara`, `pura`) VALUES
-	(3, 'CIMEKAR', 2147483647, 12312312, 12312312, 123123, 123123, 123123);
+	(1, 'SUKAINDAH', 8, 12, 0, 0, 0, 0),
+	(3, 'SUKAMURNI', 10, 20, 0, 0, 0, 0),
+	(4, 'SUKAKARYA', 4, 10, 0, 0, 0, 0),
+	(5, 'SUKALAKSANA', 6, 19, 0, 0, 0, 0),
+	(6, 'SUKAJADI', 4, 16, 0, 0, 0, 0),
+	(7, 'SUKAKARSA', 2, 4, 0, 0, 0, 0),
+	(8, 'SUKAMAKMUR', 7, 18, 0, 0, 0, 0);
 /*!40000 ALTER TABLE `masjid` ENABLE KEYS */;
 
 -- Dumping structure for table kua.penyuluh
@@ -103,13 +118,19 @@ CREATE TABLE IF NOT EXISTS `penyuluh` (
   `pendidikan` varchar(50) NOT NULL,
   `tugas` varchar(200) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_penyuluh`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
--- Dumping data for table kua.penyuluh: ~1 rows (approximately)
+-- Dumping data for table kua.penyuluh: ~7 rows (approximately)
 /*!40000 ALTER TABLE `penyuluh` DISABLE KEYS */;
 INSERT INTO `penyuluh` (`id_penyuluh`, `nama`, `jk`, `tmpt_lahir`, `tgl_lahir`, `pendidikan`, `tugas`) VALUES
-	(1, 'REY', 'L', '1', '2222-02-22', '123', '123'),
-	(5, 'GAVIN', 'L', 'cek', '2222-02-22', 'cek', 'cek');
+	(1, 'Akbar Sobari, SHI.', 'L', 'Bekasi', '1982-06-12', 'S1', 'Pemberdayaan Wakaf'),
+	(5, 'H. IQTAUFIQ SA`BANY, Lc', 'L', 'Sukatani', '1986-04-27', 'S1', 'Pemberantasan Buta Aksara Al-Qur`an'),
+	(6, 'JENAL ABIDIN, S.Sos.l.', 'L', 'Pebayuran', '1972-07-01', 'S1', 'Kerukunan Umat Beragama'),
+	(7, 'TONO KARTAWIJAYA, SHI.', 'L', 'Bekasi', '1971-12-15', 'S1', 'Keluarga Sakinah'),
+	(8, 'KIKI ZAKIYAH, S.pd.l', 'P', 'Bekasi', '1984-01-28', 'S1', 'Jaminan Produk Halal'),
+	(9, 'AHMAD HANAFI, S.Pd.l', 'L', 'Bekasi', '1967-05-16', 'S1', 'Radikalisme Dan Aliran Sempalan'),
+	(10, 'FAUZI RIDWANSYAH, S.Sos.L', 'L', 'Bekasi', '1976-05-12', 'S1', 'Penyalahgunaan Narkoba dan HIV/AIDS'),
+	(11, 'TATU MUHIMMALIYAH, S.Pd.l', 'P', 'Sukatani', '1989-05-22', 'S1', 'Pengelolaan Zakat');
 /*!40000 ALTER TABLE `penyuluh` ENABLE KEYS */;
 
 -- Dumping structure for table kua.periode
@@ -120,13 +141,16 @@ CREATE TABLE IF NOT EXISTS `periode` (
   `awal` varchar(50) DEFAULT NULL,
   `akhir` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`kode_kepala`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
 
--- Dumping data for table kua.periode: ~2 rows (approximately)
+-- Dumping data for table kua.periode: ~5 rows (approximately)
 /*!40000 ALTER TABLE `periode` DISABLE KEYS */;
 INSERT INTO `periode` (`kode_kepala`, `nomor`, `nama`, `awal`, `akhir`) VALUES
-	(16, 1, 'BERAT', 'Mei 2020', 'Januari 2021'),
-	(18, 2, 'PRIA', 'Mei 2019', 'Januari 2021');
+	(16, 1, 'Drs. H.Bunyamin', 'Juni 2004', 'Mei 2009'),
+	(18, 2, 'H. Anas Yazid Fatoni, S.Ag', 'Mei 2009', 'April 2012'),
+	(19, 3, 'H. Amir Mahmud, S.Ag', 'Mei 2012', 'April 2016'),
+	(20, 4, 'Drs. H. Komarudin, MM', 'Mei 2016', 'September 2019'),
+	(21, 5, 'Drs. H. Nanun Abdillah', 'Oktober 2019', 'Sekarang');
 /*!40000 ALTER TABLE `periode` ENABLE KEYS */;
 
 -- Dumping structure for table kua.statistik
@@ -144,7 +168,7 @@ CREATE TABLE IF NOT EXISTS `statistik` (
 -- Dumping data for table kua.statistik: ~1 rows (approximately)
 /*!40000 ALTER TABLE `statistik` DISABLE KEYS */;
 INSERT INTO `statistik` (`id_statistik`, `total_rt`, `total_rw`, `total_penduduk`, `total_penganut`, `total_tmptibadah`, `total_pendidikan`) VALUES
-	(1, '60000000', '200000000', '500000', '7777777', '50000', '777777');
+	(1, '91', '40', '53211', '63290', '134', '120');
 /*!40000 ALTER TABLE `statistik` ENABLE KEYS */;
 
 -- Dumping structure for table kua.struktur_organisasi
